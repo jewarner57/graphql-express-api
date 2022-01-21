95a6f0f3580a09d44f6ad0fd0f8174b97788af9a
Jonathan Warner <jewarner57@gmail.com>
2022-01-20 20:27:01 -0500

    feat: complete challenge 1 and 2

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 
+// Challenge List:
+// https://github.com/Tech-at-DU/ACS-4390-Query-Languages/blob/master/Lessons/Lesson-2.md
+
+const SongList = [
+  { name: 'Rain is Falling', artist: 'ELO', album: 'Time', songId: 1 },
+  { name: 'Buildings Have Eyes', artist: 'ELO', ablum: 'Secret Messages', songId: 2 },
+  { name: 'Dont Bring Me Down', artist: 'ELO', ablum: 'Discovery', songId: 3 },
+  { name: 'Turn to Stone', artist: 'ELO', ablum: 'Out of the Blue', songId: 4 },
+]
+
 // Define the Schema
 const schema = buildSchema(`
 type About {
@@ -10,6 +20,20 @@ type About {
 
 type Query {
   getAbout: About
+}
+
+enum Album {
+  Time
+  Secret Messages
+  Discovery
+  Out of the Blue
+}
+
+type Song {
+  name: String!
+  artist: String!
+  album: Album!
+  songId: Int!
 }
 `)
 
