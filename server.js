73b1e9823ef81bd2bdb75a9c44420477e72b91bc
Jonathan Warner <jewarner57@gmail.com>
2022-01-21 12:35:22 -0500

    feat: complete through challenge 12

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ type About {
 
 type Query {
   getAbout: About
+  allSongs: [Song!]!
+  getSong(songId: Int!): Song
+  firstSong: Song!
+  lastSong: Song!
+  getTime: Time
+  getRandom(range: Int!): Int!
+  getRoll(sides: Int!, rolls: Int!): Roll!
+}
+
+type Roll {
+  total: Int!
+  sides: Int!
+  rolls: [Int]!
 }
 
 enum Album {
@@ -29,18 +42,56 @@ enum Album {
   Out of the Blue
 }
 
+type Time {
+  hour: String!
+  minute: String!
+  second: String!
+}
+
 type Song {
   name: String!
   artist: String!
   album: Album!
   songId: Int!
 }
+
 `)
 
 // Define a Resolver
 const root = {
   getAbout: () => {
     return { message: 'Hello World' }
+  },
+  allSongs: () => {
+    return SongList
+  },
+  getSong: ({ songId }) => {
+    return SongList[songId]
+  },
+  firstSong: () => {
+    return SongList[0]
+  },
+  lastSong: () => {
+    return SongList[SongList.length - 1]
+  },
+  getTime: () => {
+    const time = new Date()
+    return { hour: time.getHours(), minute: time.getMinutes(), second: time.getSeconds() }
+  },
+  getRandom: ({ range }) => {
+    return parseInt(Math.round(Math.random() * range))
+  },
+  getRoll: ({ sides, rolls }) => {
+    const rollArr = []
+    let total = 0
+
+    for (i in [...Array(rolls).keys()]) {
+      const rollAmnt = Math.ceil(Math.random() * sides)
+      rollArr.push(rollAmnt)
+      total += rollAmnt
+    }
+
+    return { total, sides, rolls: rollArr }
   }
 }
 
